1a0c0e9d51d6656597dd3348524b5c43294fb807
Lise Karoliussen Haugen <lhaugen@capgemini.com>
2017-02-14 08:36:03 +0100

    added the use of bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+		  <div class="container">
+			<div class="row"> 
+			  <h1>Hello {{name}}</h1>
+			  
+			</div>
+		  </div>
+		  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+	name = 'Angular'; 
+}
